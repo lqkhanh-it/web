07524b0994af03658940797b833b04c6d0b6df38
lqkhanh-it <74171789+lqkhanh-it@users.noreply.github.com>
2020-12-10 12:06:39 +0700

    lqkhanh

---
+++ 3d.js
@@ -0,0 +1,19 @@
+// const img = document.querySelector('.card')
+// const container = document.querySelector('.card')
+// console.log(img)
+// img.style.transform = `rotateY(0deg) rotateX(50deg)`;
+// container.addEventListener("mousemove", (e) => {
+
+//     img.style.transition = "all 0.3s ease";
+//     img.style.transform = `rotateY(0deg) rotateX(0deg)`;
+// });
+// container.addEventListener("mouseenter", (e) => {
+//     img.style.transition = "none";
+
+// });
+
+// container.addEventListener("mouseleave", (e) => {
+//     img.style.transition = "all 0.3s ease";
+//     img.style.transform = `rotateY(0deg) rotateX(50deg)`;
+
+// });
